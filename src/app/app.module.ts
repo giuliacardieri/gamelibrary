78fe1869a004270f0e8f259ce3c8ea26a3b645b3
Giulia Cardieri <giuliacardieri@Giulias-MacBook-Air.local>
2018-02-18 02:42:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// browser animations
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Material elements
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
