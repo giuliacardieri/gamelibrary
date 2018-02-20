467f6b0782441b9d0eb29087a0767c810aed1aa4
Giulia Cardieri <giuliacardieri@Giulias-MacBook-Air.local>
2018-02-20 02:10:13 -0300

    fixed comma error on app-material.module.ts

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 // module to add all material elements from this web app
 
 // Material elements
-import { MatToolbarModule, MatTabsModule, MatNativeDateModule
+import { MatToolbarModule, MatTabsModule, MatNativeDateModule,
       MatCardModule, MatIconModule, MatButtonModule, 
       MatMenuModule, MatFormFieldModule, MatInputModule, 
       MatCheckboxModule, MatSelectModule, MatDatepickerModule, } from '@angular/material';
